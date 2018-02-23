b0d19b1e14c8da47cc59f468e89a058a69cb1229
Rich <rich@rickelobe.com>
2018-02-23 23:41:45 +0000

    - moved filename to description and fixed function in octoapi

--- octoapi.py
+++ octoapi.py
@@ -135,7 +135,7 @@ def get_printTimeLeft():
 	return get_job_dict()["progress"]["printTimeLeft"]
 
 def get_printFileName():
-        return get_job_dict()["file"]["name"]
+        return get_job_dict()["job"]["file"]["name"]
 
 def get_sd_dict():
 	kwargs = {"url": "printer/sd"}
--- r0b3bot.py
+++ r0b3bot.py
@@ -62,7 +62,7 @@ async def printpic(ctx):
     os.remove(full_file_name)
    
     try:
-        print_filename = octoapi.print_FileName()
+        print_filename = octoapi.get_printFileName()
     except:
         print_filename = "Unknown"
 
@@ -98,8 +98,7 @@ async def printpic(ctx):
     time_remaining = "%s Hours %s Minutes" % (print_hoursleft, print_minleft)
     #print_completion = "%s%" % (print_completion)
 
-    embed = discord.Embed(title="R0b3's 3D Printer Status", description="", color=0xF5A623)
-    embed.add_field(name="Job File: ", value=print_filename)
+    embed = discord.Embed(title="R0b3's 3D Printer Status", description="File Name: " + print_filename, color=0xF5A623)
     embed.add_field(name="Percent Complete: ", value=str(print_completion))
     embed.add_field(name="Time Elapsed: ", value=time_elapsed)
     embed.add_field(name="Time Remaining: ", value=time_remaining)
