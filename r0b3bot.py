f3e62bfb31cc34296ee1283205e9e900daff533d
Rich <rich@rickelobe.com>
2018-02-24 03:38:35 +0000

    added a missing word in a message to a user

--- r0b3bot.py
+++ r0b3bot.py
@@ -67,7 +67,7 @@ async def printstat(ctx):
             remote.call_service(hassapi, 'switch', 'turn_on', {'entity_id':'{}'.format('switch.work_lights')})
         except:
             pass
-        await ctx.send("Woah, it's pretty dark Rich's basement... Give me a few secs to turn on a light.")
+        await ctx.send("Woah, it's pretty dark in Rich's basement... Give me a couple secs to turn on a light.")
         time.sleep(3)
 
     file_name = random.randrange(1,10000)
