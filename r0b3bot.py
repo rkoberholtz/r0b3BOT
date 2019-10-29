14f167ceb7c240b3496d7157b5c266dd0758ad03
unknown <rich@RiDesktop-WIN.home.rickelobe.com>
2019-10-29 16:21:43 -0400

    uncomment else statement

--- r0b3bot.py
+++ r0b3bot.py
@@ -90,9 +90,9 @@ async def bitch(ctx):
         print(" - Joining voice channel")
         await voice.move_to(channel)
 
-    #else:
-    #    print(" ! Dunno what's going on, but it's not good")
-    #    voice = await channel.connect()
+    else:
+        print(" ! Dunno what's going on, but it's not good")
+        voice = await channel.connect()
     #source = FFmpegPCMAudio('./monkey_bitch.mp3')
 
     print(" - Creating Player")
