6815141138d977e27bd06dc95c31abddddff9c79
unknown <rich@RiDesktop-WIN.home.rickelobe.com>
2019-11-01 15:57:19 -0400

    fixed typo

--- r0b3bot.py
+++ r0b3bot.py
@@ -91,7 +91,7 @@ async def boom(ctx, member : discord.Member="NONE"):
 @bot.command()
 async def rs(ctx, member : discord.Member="NONE"):
     soundfiles = ["./sounds/monkey_bitch.mp3","./sounds/More_cowbell.mp3","./sounds/BoomBitch.mp3","./sounds/Promoted.mp3","./sounds/Trololo.mp3","./sounds/Oops.mp3"]
-    await play_sound(ctx, member, random.choice(soundFiles), "$rs")
+    await play_sound(ctx, member, random.choice(soundfiles), "$rs")
 
 async def play_sound(ctx, member : discord.Member, soundFile, command):
     discord.opus.load_opus("libopus.so")
