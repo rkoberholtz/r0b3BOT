5b7448da621dad27179af999322a35a6336687de
Rich <rich@rickelobe.com>
2019-12-05 14:22:54 -0500

    added % sign

--- r0b3bot.py
+++ r0b3bot.py
@@ -392,7 +392,7 @@ async def printstat(ctx):
         embed.add_field(name="Hotend Temp.: ", value=printer_hotend)
         print(" - Sending details to chat channel")
         await ctx.send(embed=embed)
-        activity = discord.Activity(name=f"3D Print @ {str(print_completion)}",type=discord.ActivityType.watching)
+        activity = discord.Activity(name=f"3D Print @ {str(print_completion)}%",type=discord.ActivityType.watching)
         await bot.change_presence(activity=activity)
 
     else:
