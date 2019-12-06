9a8c480dc33afa5af7935adebc240948b0a257c5
Rich Oberholtzer <rich@rickelobe.com>
2019-12-05 21:00:50 -0500

    removed sleep to

--- r0b3bot.py
+++ r0b3bot.py
@@ -430,13 +430,13 @@ async def updateStatus():
     # Wait 10 minutes before changing the status back to listening to your commands, only if uknowns !5.
     if unknowns != 5:
         #await asyncio.sleep(600)
-        for x in range(10):
+        for x in range(100):
             activity = discord.Activity(name=f"3D Print @ {str(print_completion)}",type=discord.ActivityType.listening)
             await bot.change_presence(activity=activity)
-            await asyncio.sleep(1)
+            #await asyncio.sleep(1)
             activity = discord.Activity(name="---------------",type=discord.ActivityType.listening)
             await bot.change_presence(activity=activity)
-            await asyncio.sleep(1)
+            #await asyncio.sleep(1)
 
     activity = discord.Activity(name="your commands",type=discord.ActivityType.listening)
     await bot.change_presence(activity=activity)
