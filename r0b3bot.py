d86f6348275fb8425dd164e0e4e0d5d4e7e59fb1
Rich Oberholtzer <rich@rickelobe.com>
2020-04-09 22:26:31 -0400

    dump to separate line

--- r0b3bot.py
+++ r0b3bot.py
@@ -87,7 +87,8 @@ async def on_command_error(ctx, error):
     if isinstance(error, commands.CommandInvokeError):
         print(" - ERROR: encountered 'CommandInvokeError'")
         print(f" - Dumping error: {error}")
-        global last_error = error
+        global last_error
+        last_error = error
         await ctx.send("Error, unable to complete your request.")
     else:
         print('Oopsie, I found an error...')
