427937e8d52f1aee409e68cfb07a97240527e420
Rich Oberholtzer <rich@rickelobe.com>
2020-04-09 23:37:50 -0400

    added $ hint in status

--- r0b3bot.py
+++ r0b3bot.py
@@ -71,7 +71,7 @@ async def on_ready():
     print(bot.user.id)
     print('------')
     print("Setting activity to 'Listenting to your commands")
-    activity = discord.Activity(name="your commands",type=discord.ActivityType.listening)
+    activity = discord.Activity(name="your $commands",type=discord.ActivityType.listening)
     await bot.change_presence(activity=activity)
 
 @bot.command()
