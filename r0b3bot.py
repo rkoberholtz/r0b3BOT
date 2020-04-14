c9c67f32c97f48984726a3ac4f2c87807ed72da7
Rich Oberholtzer <rich@rickelobe.com>
2020-04-13 21:05:53 -0400

    typo?

--- r0b3bot.py
+++ r0b3bot.py
@@ -668,7 +668,7 @@ async def spsub(ctx, service = "NONE"):
                         # Append the current channel id to the list for this service
                         print(f">> Adding serivce '{currentsub_request[1]}' to {ctx.channel.id}")
                         await ctx.send(f"{service} has been added to monitored services for this channel")
-                        spsublist.update([currentsub_request[1]] = {'state' : 'online', 'channels' : [currentsub_request[0]]})
+                        spsublist.update(currentsub_request[1] = {'state' : 'online', 'channels' : [currentsub_request[0]]})
                         #spsublist[service]['channels'].append(currentsub_request[0])
                     
             else:
