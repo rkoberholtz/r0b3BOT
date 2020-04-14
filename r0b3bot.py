0bf0134a8645e35fcbd33be291fb8c89e9a02b8b
Rich Oberholtzer <rich@rickelobe.com>
2020-04-13 20:59:54 -0400

    changin variable

--- r0b3bot.py
+++ r0b3bot.py
@@ -666,7 +666,7 @@ async def spsub(ctx, service = "NONE"):
                     
                     if not found_service:
                         # Append the current channel id to the list for this service
-                        print(f">> Adding serivce '{currentsub_request[1]}' to {channel}")
+                        print(f">> Adding serivce '{currentsub_request[1]}' to {ctx.channel.id}")
                         await ctx.send(f"{service} has been added to monitored services for this channel")
                         spsublist[service]['channels'].append(currentsub_request[0])
                     
