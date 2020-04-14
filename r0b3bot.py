8f9af7d10c70e255870ee81471fc20303eb93bfc
Rich Oberholtzer <rich@rickelobe.com>
2020-04-13 21:48:57 -0400

    addd -del option

--- r0b3bot.py
+++ r0b3bot.py
@@ -618,7 +618,30 @@ async def spsub(ctx, service = "NONE"):
         elif service.startswith('-del '):
             # Delete the service from this channels subscriptions
             print("placeholder")
+            service_toremove = service[5:]
+            print(f"{service_toremove}")
 
+            async with aiof.open('spsublist.dat', 'rb') as datafile:
+                pickled_spsublist = await datafile.read()
+                spsublist = pickle.loads(pickled_spsublist)
+            
+            found_service = False
+            for service in spsublist.keys():
+                
+                if service.lower() == service_toremove.lower():
+                    found_service = True
+                    found_channel = False
+                    service_toremove = serivce
+                    for channel in spsublist[service]['channels']:
+                        if channel == ctx.channel.id:
+                            found_service = True
+                            spsublist[service]['channels'].remove(channel)
+
+            if found_service and found_channel:
+
+                await ctx.send(f"This channel is unsubscribed from '{service}' alerts.")
+            else:
+                await ctx.send(f"This channel is not subscribed to alerts for '{service_toremove}")
 
         # If the service exists, proceed with adding it to the list.
         elif service_state != "service not found":
