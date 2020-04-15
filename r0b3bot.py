f11c0762589be1879ab3db280709760633e69ee3
Rich Oberholtzer <rich@rickelobe.com>
2020-04-14 21:52:41 -0400

    trying removal of print statement

--- r0b3bot.py
+++ r0b3bot.py
@@ -599,13 +599,7 @@ async def spsub(ctx, service = "NONE"):
     datestring = datetime.now()
     datestring = datestring.strftime("%m/%d/%Y-%H:%M:%S")
 
-    #bad_chars = ['"'] 
-  
-    # using replace() to remove bad_chars  
-    #for i in bad_chars : 
-    service = service.replace('"', '') 
-
-    print(f"[{datestring}]: {ctx.message.author.display_name} called '$spsub '{service}'''")
+    #print(f"[{datestring}]: {ctx.message.author.display_name} called '$spsub '{service}'''")
 
     # Only work on this if the user has supplied a service name to monitor, a value of NONE
     #  means nothing was specified
